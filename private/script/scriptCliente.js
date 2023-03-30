b17e7c4484e96c3309acd4de8f309fe0c5f1c78e
carvalhotkeys <vendascarvalhoti@gmail.com>
2023-03-30 14:19:50 -0300

    add funcoes
    add funcoes e botoes

--- private/script/scriptCliente.js
+++ private/script/scriptCliente.js
@@ -35,8 +35,26 @@ function exibirClienteFormaTabela(listaClientes){
                           <td>${listaClientes[i].instrumento}</td>
                           <td>${listaClientes[i].uf}</td>
                           <td>
-                            <button type="button" class="btn btn-warning">Editar</button>
-                            <button type="button" class="btn btn-danger">Excluir</button>
+                            <button onclick="selecionarRegistro('${listaClientes[i].cpf}',
+                                                                '${listaClientes[i].nome}',
+                                                                '${listaClientes[i].endereco}',
+                                                                '${listaClientes[i].bairro}',
+                                                                '${listaClientes[i].cidade}',
+                                                                '${listaClientes[i].email}',
+                                                                '${listaClientes[i].telefone}',
+                                                                '${listaClientes[i].instrumento}',
+                                                                '${listaClientes[i].uf}',
+                                                                'editar')" type="button" class="btn btn-warning">Editar</button>
+                            <button onclick="selecionarRegistro('${listaClientes[i].cpf}',
+                                                                '${listaClientes[i].nome}',
+                                                                '${listaClientes[i].endereco}',
+                                                                '${listaClientes[i].bairro}',
+                                                                '${listaClientes[i].cidade}',
+                                                                '${listaClientes[i].email}',
+                                                                '${listaClientes[i].telefone}',
+                                                                '${listaClientes[i].instrumento}',
+                                                                '${listaClientes[i].uf}',
+                                                                'excluir')" type="button" class="btn btn-danger">Ecluir</button>
                           </td>`
         corpo.appendChild(linha);
                           
@@ -103,6 +121,9 @@ function gravarClienteBackend(){
             mensagem.innerHTML = `<div class="alert alert-success" role="alert">
                                         ${dados.mensagem}
                                  </div>`
+            limpaFormulario()
+            location.reload();
+            obterClientesBackend()
         }
         else{
             mensagem.innerHTML = `<div class="alert alert-danger" role="alert">
@@ -195,6 +216,157 @@ function validarCliente(){
 
     mensagem.innerHTML="";
         return true;
+}
+
+function selecionarRegistro(pcpf, pnome, pendereco, pbairro, pcidade, pemail, ptelefone, pinstrumento, puf, acao){
+    let cpf = document.getElementById("cpf");
+    let nome = document.getElementById("nome");
+    let endereco = document.getElementById("endereco");
+    let bairro = document.getElementById("bairro");
+    let cidade = document.getElementById("cidade");
+    let email = document.getElementById("email");
+    let telefone = document.getElementById("telefone");
+    let instrumento = document.getElementById("instrumento");
+    let uf = document.getElementById("uf");
+
+    cpf.value = pcpf;
+    nome.value = pnome;
+    endereco.value = pendereco;
+    bairro.value = pbairro;
+    cidade.value = pcidade;
+    email.value = pemail;
+    telefone.value = ptelefone;
+    instrumento.value = pinstrumento;
+    uf.value = puf;
+
+    let btnCadastro = document.getElementById("cadastrar");
+    let btnEditar = document.getElementById("editar");
+    let btnExcluir = document.getElementById("excluir");
+
+    if (acao === "excluir"){
+        document.getElementById('cpf').disabled = true;
+        btnCadastro.disabled = true;
+        btnEditar.disabled = true;
+        btnExcluir.disabled = false;
+    }
+    else if (acao === "editar"){
+        document.getElementById('cpf').disabled = true;
+        btnCadastro.disabled = true;
+        btnEditar.disabled = false;
+        btnExcluir.disabled = true;
+    }
+    else{
+        document.getElementById('cpf').disabled = false;
+        btnCadastro.disabled = false;
+        btnEditar.disabled = true;
+        btnExcluir.disabled = true;
+        limpaFormulario();
+    }
+
+}
+
+function limpaFormulario(){
+    document.getElementById("cpf").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("endereco").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("telefone").value = "";
+    document.getElementById("instrumento").value = "";
+    document.getElementById("uf").value = "";
+
+}
+
+function apagarCliente(){
+    if(confirm("Você confirma a exclusão do cliente selecionado? ")){
+        fetch(urlBackend, {
+            method:"DELETE",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                cpf: document.getElementById('cpf').value
+            })
+        }).then((resposta)=>{
+            if(resposta.ok) return resposta.json();
+        }).then((dados)=>{
+            mensagem.innerHTML = `<div class="alert alert-success" role="alert">
+                                        ${dados.mensagem}
+                                 </div>`
+            limpaFormulario();
+            location.reload();
+            obterClientesBackend();
+        }).catch((erro)=>{
+            mensagem.innerHTML = `<div class="alert alert-danger" role="alert">
+                                    ${erro.message}
+                              </div>`
+        });
+    }
+    else{
+        limpaFormulario();
+        location.reload();
+        obterClientesBackend();
+    }
+}
+
+function atualizarCliente(){
+    if(confirm("Você confirma a atualização do cliente? ")){
+        let cpf = document.getElementById("cpf").value;
+        let nome = document.getElementById("nome").value;
+        let endereco = document.getElementById("endereco").value;
+        let bairro = document.getElementById("bairro").value;
+        let cidade = document.getElementById("cidade").value;
+        let email = document.getElementById("email").value;
+        let telefone = document.getElementById("telefone").value;
+        let instrumento = document.getElementById("instrumento").value;
+        let uf = document.getElementById("uf").value;
+        let mensagem = document.getElementById("mensagem");
+    
+        fetch(urlBackend,{
+            method:"PUT",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                "cpf":cpf,
+                "nome":nome,
+                "endereco":endereco,
+                "bairro":bairro,
+                "cidade":cidade,
+                "email":email,
+                "telefone":telefone,
+                "instrumento":instrumento,
+                "uf":uf,
+            })
+        }).then((resposta)=>{  //then == entao
+            return resposta.json();
+        }).then((dados)=>{
+            if(dados.status){
+                obterClientesBackend();
+                mensagem.innerHTML = `<div class="alert alert-success" role="alert">
+                                            ${dados.mensagem}
+                                     </div>`
+                limpaFormulario();
+                location.reload();
+                obterClientesBackend();
+            }
+            else{
+                mensagem.innerHTML = `<div class="alert alert-danger" role="alert">
+                                            ${dados.mensagem}
+                                      </div>`
+            }
+        }).catch((erro)=> {
+            mensagem.innerHTML = `<div class="alert alert-danger" role="alert">
+                                        ${erro.message}
+                                  </div>`
+        });
+    }
+    else{
+        limpaFormulario();
+        location.reload();
+        obterClientesBackend();
+    }
 }
 
 obterClientesBackend();
